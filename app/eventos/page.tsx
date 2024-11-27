bea48f8215c20aa0d18b4c4dd04d0c91590c12d6
Geraldiene <riomahabramromamateo@gmail.com>
2024-11-27 09:27:23 -0400

    vvvvv

--- app/eventos/page.tsx
+++ app/eventos/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import Navbar from '@/Components/ui/Navbar'
+import Navbar from '@/components/ui/Navbar'
+import Eventos from '@/components/Eventos'
+import PortadaEvento from '@/components/PortadaEvento'
+
 const eventos = () => {
   return (
     <div>
       <Navbar/>
-      eventos
+      <PortadaEvento/>
+      <Eventos/>
       </div>
   )
 }
